2d9c0c59edae876166054ebd324a0563e11cf0b2
Kiren Deloney Brumfield <kb1559977@ebrschools.org>
2025-03-07 08:24:18 -0600

    added new spell

---
+++ Player/Wanderer Magican/Skills/light/light_shield.gd
@@ -0,0 +1,23 @@
+extends Area2D
+var speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemies"):
+		
+		
+		pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+	pass # Replace with function body.
