35be1bb0f5bb607b2a0dcd25d37ef4c392bc5e84
Kiren Deloney Brumfield <kb1559977@ebrschools.org>
2025-03-17 08:38:39 -0500

    nightly push

--- Player/Wanderer Magican/Skills/ice/ice_shot.gd
+++ Player/Wanderer Magican/Skills/ice/ice_shot.gd
@@ -2,7 +2,7 @@ extends Area2D
 var speed = 200
 var speed_mod = 1.5
 var direction = Vector2.ONE
-
+@export var damage: int = 100 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,7 +19,12 @@ func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("Player"):
 		$AnimatedSprite2D.play("Impact")
 		speed = 0
-		pass # Replace with function body.
+	
+	if body is CharacterBody2D and body.has_method("take_damage") and body.is_in_group("Enemies"):
+		body.take_damage(damage)
+		queue_free()
+	queue_free()
+	pass # Replace with function body.
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
