1c4dacafc0c23b8a5dd50327d6334aaf1c270fe7
Rishard Wright <rw206400@ebrschools.org>
2025-04-04 08:47:08 -0500

    Finished selection screen

--- player_select.gd
+++ player_select.gd
@@ -1,4 +1,7 @@
 extends Node2D
+@onready var wizard_class: Sprite2D = $"Wizard Class"
+@onready var knight_class: Sprite2D = $"Knight Class"
+@onready var ranger_class: Sprite2D = $"Ranger Class"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +11,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
